f440a6eababb83cb1c10167aa6464aecb9d9a886
HarryNwa <harrynwa90@gmail.com>
2023-11-17 08:21:00 +0100

    update

---
+++ asset/mailer.js
@@ -0,0 +1,8 @@
+const fullName = document.getElementById("fullName");
+const title = document.getElementById("title");
+const sender = document.getElementById("SenderEmail");
+const content = document.getElementById("content");
+
+function SenderEmail(){
+    
+}
